f8dca67710f48fc8447911d3e8c3fd72c37a3844
Florian <info@codinginflow.com>
2022-12-22 11:42:21 +0100

    Part 4 done

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,29 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import NoteModel from "./models/note"
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
+app.get("/", async (req, res, next) => {
+    try {
+        // throw Error("Bazinga!");
+        const notes = await NoteModel.find().exec();
+        res.status(200).json(notes);
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.use((req, res, next) => {
+    next(Error("Endpoint not found"));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(error);
+    let errorMessage = "An unknown error occurred";
+    if (error instanceof Error) errorMessage = error.message;
+    res.status(500).json({ error: errorMessage });
 });
 
 export default app;
---
+++ backend/src/models/note.ts
@@ -0,0 +1,10 @@
+import { InferSchemaType, model, Schema } from "mongoose";
+
+const noteSchema = new Schema({
+    title: { type: String, required: true },
+    text: { type: String },
+}, { timestamps: true });
+
+type Note = InferSchemaType<typeof noteSchema>;
+
+export default model<Note>("Note", noteSchema);
