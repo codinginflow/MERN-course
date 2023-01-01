147cebf3fb72be4b43ba5db89631a99a2d4f60a9
Florian <info@codinginflow.com>
2023-01-01 11:26:17 +0100

    End of part 7

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -56,3 +56,65 @@ export const createNote: RequestHandler<unknown, unknown, CreateNoteBody, unknow
         next(error);
     }
 };
+
+interface UpdateNoteParams {
+    noteId: string,
+}
+
+interface UpdateNoteBody {
+    title?: string,
+    text?: string,
+}
+
+export const updateNote: RequestHandler<UpdateNoteParams, unknown, UpdateNoteBody, unknown> = async(req, res, next) => {
+    const noteId = req.params.noteId;
+    const newTitle = req.body.title;
+    const newText = req.body.text;
+
+    try {
+        if (!mongoose.isValidObjectId(noteId)) {
+            throw createHttpError(400, "Invalid note id");
+        }
+
+        if (!newTitle) {
+            throw createHttpError(400, "Note must have a title");
+        }
+
+        const note = await NoteModel.findById(noteId).exec();
+
+        if (!note) {
+            throw createHttpError(404, "Note not found");
+        }
+
+        note.title = newTitle;
+        note.text = newText;
+
+        const updatedNote = await note.save();
+
+        res.status(200).json(updatedNote);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const deleteNote: RequestHandler = async(req, res, next) => {
+    const noteId = req.params.noteId;
+
+    try {
+        if (!mongoose.isValidObjectId(noteId)) {
+            throw createHttpError(400, "Invalid note id");
+        }
+
+        const note = await NoteModel.findById(noteId).exec();
+
+        if (!note) {
+            throw createHttpError(404, "Note not found");
+        }
+
+        await note.remove();
+        
+        res.sendStatus(204);
+    } catch (error) {
+      next(error);  
+    }
+};
--- backend/src/routes/notes.ts
+++ backend/src/routes/notes.ts
@@ -9,4 +9,8 @@ router.get("/:noteId", NotesController.getNote);
 
 router.post("/", NotesController.createNote);
 
+router.patch("/:noteId", NotesController.updateNote);
+
+router.delete("/:noteId", NotesController.deleteNote);
+
 export default router;
