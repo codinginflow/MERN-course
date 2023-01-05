9b6e39591749030ffc831bacb84f3a55915c5c95
Florian <info@codinginflow.com>
2023-01-05 12:10:26 +0100

    Added missing requiresAuth

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -29,7 +29,7 @@ app.use(session({
 }));
 
 app.use("/api/users", userRoutes);
-app.use("/api/notes",  notesRoutes);
+app.use("/api/notes", requiresAuth, notesRoutes);
 
 app.use((req, res, next) => {
     next(createHttpError(404, "Endpoint not found"));
